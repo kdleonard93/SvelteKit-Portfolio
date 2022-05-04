e27c5a1ed83c1ed6609f2053c8fa42ceebaa1d43
Kyle <kdleonard@icloud.com>
2022-05-04 12:56:26 -0500

    gql client seperation

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,38 +1,34 @@
 <script context="module">
-    import { gql, GraphQLClient } from 'graphql-request'
-  
-    export const load = async () => {
-      const client = new GraphQLClient(
-        import.meta.env.VITE_GRAPHQL_API
-      )
+  import {client} from "/lib/client.js";
+	import { gql } from 'graphql-request';
 
-      const query = gql`
-      query GetProjects {
-        projects {
-          name
-          slug
-          description
-          demo
-          sourceCode
-          image {
-            url
-          }
-        }
-      }
-    `
+		const query = gql`
+			query GetProjects {
+				projects {
+					name
+					slug
+					description
+					demo
+					sourceCode
+					image {
+						url
+					}
+				}
+			}
+		`;
 
-    const { projects } = await client.request(query)
+		const { projects } = await client.request(query);
 
-    return {
-        props: {
-            projects,
-        },
-    }
-  }
-  </script>
+		return { 
+			props: { 
+				projects
+			}
+		};
+	};
+</script>
 
 <script>
-  export let projects;
+	export let projects;
 </script>
 
 <pre>{JSON.stringify(projects, null, 2)}</pre>

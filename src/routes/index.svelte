5b3b873aaed2258ac5eb5969a5cbf19d841e6bf1
Kyle <kdleonard@icloud.com>
2022-05-31 16:34:22 -0500

    Updated Project Card Component and added basic styling with tailwind

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,40 +1,41 @@
 <script context="module">
-import ProjectCard from '$lib/components/project-card.svelte'
-  import {client} from "/lib/client.js";
-	import { gql } from 'graphql-request';
-
-		const query = gql`
-			query GetProjects {
-				projects {
-					name
-					slug
-					description
-					demo
-					sourceCode
-					image {
-						url
-					}
-				}
+	import ProjectCard from '$lib/components/project-card.svelte'
+	import { client } from '$lib/graphql-client'
+	import { gql } from 'graphql-request'
+  
+	export const load = async () => {
+	  const query = gql`
+		query GetProjects {
+		  projects {
+			name
+			slug
+			description
+			tags
+			demo
+			sourceCode
+			image {
+			  url
 			}
-		`;
-
-		const { projects } = await client.request(query);
-
-		return { 
-			props: { 
-				projects
-			}
-		};
-	};
-</script>
-
-<script>
-	export let projects;
-</script>
-
-<h1>{JSON.stringify(projects, null, 2)}</h1>
-
-<div>
+		  }
+		}
+	  `
+	  const { projects } = await client.request(query)
+  
+	  return {
+		props: {
+		  projects,
+		},
+	  }
+	}
+  </script>
+  
+  <script>
+	export let projects
+  </script>
+  
+  <h1>Recent Projects by Me</h1>
+  
+  <div>
 	{#each projects as { name, slug, description, image }}
 	  <ProjectCard {name} {description} url={image[0].url} {slug} />
 	{/each}
